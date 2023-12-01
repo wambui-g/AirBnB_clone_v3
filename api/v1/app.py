2eeea220b202a1de75cb93dbf61ef3a3d315be3b
wambui-g <grayciekamau03@gmail.com>
2023-12-01 14:41:17 +0300

    404

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """application api"""
 import os
-from flask import Flask
+from flask import Flask, jsonify
 
 from models import storage
 from api.v1.views import app_views
@@ -10,7 +10,7 @@ from api.v1.views import app_views
 app = Flask(__name__)
 """instance of web application"""
 app_host = os.getenv("HBNB_API_HOST", "0.0.0.0")
-app_port = int(os.getenv("HBNB_API+PORT", "5000"))
+app_port = int(os.getenv("HBNB_API_PORT", "5000"))
 app.register_blueprint(app_views)
 app.url_map.strict_slashes = False
 
@@ -19,6 +19,12 @@ app.url_map.strict_slashes = False
 def teardown_flask(exception):
     """end event listener"""
     storage.close()
+
+
+@app.errorhandler(404)
+def error_404(error):
+    """404 http error"""
+    return jsonify(error="Not found"), 404
 
 
 if __name__ == "__main__":
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,23 @@
 #!/usr/bin/python3
+"""index view"""
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
-app_views.route('/status', methods=['GET'], strict_slashes=False)
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """returns json"""
     return jsonify(status="OK")
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stat():
+    """returns no. of objects by type"""
+    return jsonify(
+        amenities=storage.count('Amenity'),
+        cities=storage.count('City'),
+        places=storage.count('Review'),
+        state=storage.count('State'),
+        users=storage.count('User')
+    )
